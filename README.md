b4b9158008de7d1d955b7f97e243cda1f5cf61f2
Yusuke Izawa <yuizalp@gmail.com>
2017-05-23 16:53:59 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# make-pdf-from-images
+
+This can make a `pdf` file from some image files.
+
+## Install
+
+``` bash
+$ bundle install --path=vendor/bundle
+```
+
+## Usage
+
+Add some image files to the current dicectory and execute the command below.
+
+``` bash
+$ bundle exec ruby <filename>
+```
+
+Then, `<filename>.pdf` would be generated.
---
+++ make-pdf-from-images.rb
@@ -0,0 +1,24 @@
+require 'fileutils'
+require 'RMagick'
+
+def mkpdf
+  jpg_files = []
+
+  Dir::foreach('.') { |f|
+    if File::ftype(f) == "file"
+      if f.end_with?(".jpg") || f.end_with?(".png") || f.end_with?(".JPG") || f.end_with?(".JPEG")
+        jpg_files.push(f.to_s)
+      end
+    end
+  }
+
+  r = Magick::ImageList.new()
+
+  jpg_files.reverse.each do |f|
+    r.push(Magick::Image.read(f)[0])
+  end
+
+  r.write("#{ARGV[0]}.pdf")
+end
+
+mkpdf
